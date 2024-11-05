75eebd4a8ad0356af9a855653db66d70d3ba08bf
Lei Song <pinot.sung@gmail.com>
2024-11-05 14:29:20 -0800

    Ready for modeling

---
+++ schedulers/select_variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -c 3
+#SBATCH --mem 30G
+#SBATCH -t 01-00:00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=lsong@ucsb.edu
+
+SP=$1
+
+conda activate sdm
+cd ~/SCImpact
+
+srun Rscript R/vs_warp.R -s $SP
--- schedulers/slurm_submit_es.R
+++ schedulers/slurm_submit_es.R
@@ -6,13 +6,24 @@ species2 <- list.files(file.path(root_dir, "variables", "variable_list"))
 species2 <- gsub(".csv", "", species2)
 species <- intersect(species, species2)
 
+species1 <- list.files(file.path(root_dir, "occurrences/CSVs_thin"))
+species1 <- gsub(".csv", "", species1)
+species2 <- list.files(file.path(root_dir, "occurrences/bg"))
+species2 <- gsub(".csv", "", species2)
+species2 <- intersect(species1, species2)
+species <- setdiff(species, species2)
+
 message(sprintf("%s species to process.", length(species)))
+
+# # If use one node
+# sp <- paste(species, collapse = ",")
+# system(sprintf("sbatch schedulers/env_sampling.sh %s", sp))
 
 chunk_length <- 100
 species_list <- split(
     species, ceiling(seq_along(species) / chunk_length))
 
-for (i in 1:2){
+for (i in 1:length(species_list)){
     sp <- species_list[[i]]
     sp <- paste(sp, collapse = ",")
     system(sprintf("sbatch schedulers/env_sampling.sh %s", sp))
